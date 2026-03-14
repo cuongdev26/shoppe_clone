a2b44c0e6701cd6163ffa3cb8821b0d0c3890670
cuongdev26 <phamcuong26.dev@gmail.com>
2026-03-14 12:14:50 +0700

    update shoppe clone

--- shoppe/src/main/java/com/shoppe/configuration/SecurityConfig.java
+++ shoppe/src/main/java/com/shoppe/configuration/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean                         cccccccccccccccccccdeddddsx xxxxxxxsc                                                                                                                                                                                                               za
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
